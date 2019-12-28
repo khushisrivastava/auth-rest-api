522faf9d003e2ca16010896f497c064be37f8878
Khushi <khushisrivastava53@gmail.com>
2019-12-29 02:28:08 +0530

    Deployment #4

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'api/users', views.UserCreate.as_view(), name='account-create'),
+    url(r'api/users', views.Register.as_view(), name='Register'),
 ]
