12a6a4e0e3c9378551dab99dfd84861fe9d33c30
Khushi <khushisrivastava53@gmail.com>
2019-12-29 02:23:56 +0530

    Deployment #3

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -3,16 +3,16 @@ from rest_framework.validators import UniqueValidator
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
-    first_name = serializers.CharField(required=True, max_length=100)
-    last_name = serializers.CharField(required=True, max_length=100)
-    #phone = serializers.CharField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
-    email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
-    username = serializers.CharField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
-    password = serializers.CharField(min_length=6, write_only=True)
+    # first_name = serializers.CharField(required=True, max_length=100)
+    # last_name = serializers.CharField(required=True, max_length=100)
+    # #phone = serializers.CharField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
+    # email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
+    # username = serializers.CharField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
+    # password = serializers.CharField(min_length=6, write_only=True)
 
-    def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
-        return user
+    # def create(self, validated_data):
+    #     user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+    #     return user
 
     class Meta:
         model = User
