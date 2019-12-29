6bdac6c30b2c7a2e6591f0ae37e1ebeb72df831c
Khushi <khushisrivastava53@gmail.com>
2019-12-29 16:36:42 +0530

    Deployment #13

--- Authentication/settings.py
+++ Authentication/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'e@45uldk@fz!0d+q=m(0ntfv($^tmbu-0!td0knpng660v$17_'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'https://git.heroku.com/powerful-crag-82238.git',
+    'powerful-crag-82238.herokuapp.com',
     'localhost',
 ]
 
