7a947a0d37ac79d982bb0274b9f7d7a10d591816
Khushi <khushisrivastava53@gmail.com>
2019-12-29 16:26:58 +0530

    Deployment #11

--- Authentication/settings.py
+++ Authentication/settings.py
@@ -107,3 +107,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
